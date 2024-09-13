b482e536695b083060e805a74046522b8109d647
Eric Curti <ericfacultad@gmail.com>
2024-09-13 14:10:37 -0600

    cart button

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">LevelUP Cart</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "❌";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () =>{
+        modalContainer.style.display = "none";
+    });
