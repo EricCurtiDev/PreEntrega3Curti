2fb40cd82804107e407684dc443665526efad9b0
Eric Curti <ericfacultad@gmail.com>
2024-09-13 13:59:03 -0600

    js folder and files

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id : 1,
+        nombre: "Dead by deadlight",
+        precio: 29,
+        img: "./assets/img/p1.jpg",
+        cantidad: 1,
+    },
+    {
+        id : 2,
+        nombre: "Grand theft auto 5",
+        precio: 59,
+        img: "./assets/img/p2.jpg",
+        cantidad: 1,
+    },
+    {
+        id : 3,
+        nombre: "Elden ring",
+        precio: 69,
+        img: "./assets/img/p3.jpg",
+        cantidad: 1,
+    },
+    {
+        id : 4,
+        nombre: "FC 24",
+        precio: 49,
+        img: "./assets/img/p4.jpg",
+        cantidad: 1,
+    },
+];
